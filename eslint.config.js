41838bfb8652d29f4ccac5135ef7f482b1737cd8
sufi <sufi@sufis-MacBook-Air.local>
2025-10-24 12:10:05 +0530

    Basic Eslint Setup

---
+++ eslint.config.js
@@ -0,0 +1,56 @@
+import js from '@eslint/js';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      globals: {
+        // Browser globals
+        window: 'readonly',
+        document: 'readonly',
+        navigator: 'readonly',
+        
+        // Jest globals
+        test: 'readonly',
+        expect: 'readonly',
+        jest: 'readonly',
+        describe: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+
+        // Node globals
+        process: 'readonly',
+        __dirname: 'readonly',
+        module: 'readonly',
+        require: 'readonly'
+      }
+    },
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-vars': 'error',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'no-unused-vars': 'warn',
+      'semi': ['error', 'always']
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.scss';
+import "./App.scss";
 
 function App() {
   return (
-    <>
+    <div>
       <h1>Hello World</h1>
-    </>
+      <p>I am here</p>
+    </div>
   );
 }
 
